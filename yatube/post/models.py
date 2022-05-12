c7b005179e96a651d7756357065879f8d30064ef
Dmitry Kosinevskiy <crecker76@gmail.com>
2022-05-12 20:38:19 +0300

    Добавлена понель админа созданы модели и связи в БД

--- yatube/post/models.py
+++ yatube/post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model() #Модель отвечающая за управление пользователями
+
+class Post(models.Model):
+    
+    text = models.TextField() # поле формата текст
+    pub_date = models.DateTimeField(auto_now_add= True) # поле даты создания поста. auth_now_add-для автоматическогоприсваения даты посту
+    
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='posts'
+        ) # полея для указания ссылки на другую модель(другую таблицу) 
+    #on_delete=models.CASCADE обеспечивает связанность данных при удаление пользователя будут удалены все посты 
